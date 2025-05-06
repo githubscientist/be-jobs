ae0703252b7a2f2ed8fb811106dd5a274532f6c3
githubscientist <sathish.datascientist@gmail.com>
2025-05-06 18:32:46 +0530

    upload --single upload route setup done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('./utils/logger');
 const cookieParser = require('cookie-parser');
 const errorRoute = require('./utils/errorRoute');
 const userRouter = require('./routes/userRoutes');
+const fileRouter = require('./routes/fileRoutes');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(logger);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/files', fileRouter);
 
 // Middleware to handle 404 errors
 app.use(errorRoute);
